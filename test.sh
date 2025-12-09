47a7ba53e650fb0585fee6fc5fd7acacdf897d50
KIRCO\wzh26 <2657751462@qq.com>
2025-12-10 03:19:48 +0800

    Initial commit: MatchMamba project

---
+++ test.sh
@@ -0,0 +1,26 @@
+cd core/
+
+python main.py --run_mode=test      \
+              --model_path=../log/train/ \
+              --res_path=../log/yfcc_know \
+              --data_te=/home/lab603/Documents/OADATA/yfcc-sift-2000-testknown.hdf5 \
+              --use_ransac=True \
+              --log_base=../log/
+python main.py --run_mode=test      \
+              --model_path=../log/train/ \
+              --res_path=../log/yfcc_know \
+              --data_te=/home/lab603/Documents/OADATA/yfcc-sift-2000-testknown.hdf5 \
+              --use_ransac=False \
+              --log_base=../log/
+python main.py --run_mode=test      \
+              --model_path=../log/train/ \
+              --res_path=../log/yfcc_unknow \
+              --data_te=/home/lab603/Documents/OADATA/yfcc-sift-2000-test.hdf5 \
+              --use_ransac=True \
+              --log_base=../log/
+python main.py --run_mode=test      \
+              --model_path=../log/train/ \
+              --res_path=../log/yfcc_unknow \
+              --data_te=/home/lab603/Documents/OADATA/yfcc-sift-2000-test.hdf5 \
+              --use_ransac=False \
+              --log_base=../log/
---
+++ train.sh
@@ -0,0 +1,9 @@
+cd core/
+
+##### yfcc-sift-2000
+python main.py --run_mode=train      \
+              --log_base=../log/     \
+              --data_tr=/home/lab603/Documents/OADATA/yfcc-sift-2000-train.hdf5 \
+              --data_va=/home/lab603/Documents/OADATA/yfcc-sift-2000-val.hdf5
+
+
